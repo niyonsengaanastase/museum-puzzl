0b264c480b4556e41f488256a4a17d8b92ea2fc5
JASON statham <jasonstatham2000n@gmail.com>
2026-06-12 14:23:33 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Game />
+  </div>
+</template>
+
+<script>
+import Game from './components/Game.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Game
+  }
+}
+</script>
+
+<style>
+body{
+  margin:0;
+  padding:0;
+}
+</style>
